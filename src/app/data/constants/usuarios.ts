7807570e918a4244eaa19e9cf77bb27a449d2fa5
Yan Luiz <yan.luiz.lima@gmail.com>
2024-08-26 22:08:26 -0300

    feat(yan) adding user registration functionally

---
+++ src/app/data/constants/usuarios.ts
@@ -0,0 +1,49 @@
+import { Usuario } from "@/core/model/Usuario";
+
+const usuarios: Usuario[] = [
+    {
+        id: "1",
+        email: "ana@empresa.com.br",
+        nome: "Ana",
+        senha: "123456"
+    },
+    {
+        id: "2",
+        email: "pedro@empresa.com.br",
+        nome: "Pedro",
+        senha: "789101112"
+
+    },
+    {
+        id: "3",
+        email: "guilherme@empresa.com.br",
+        nome: "Guilherme",
+        senha: "78910111"
+
+    },
+    {
+        id: "4",
+        email: "augusto@empresa.com.br",
+        nome: "Augusto",
+        senha: "789101112"
+
+    },
+    {
+        id: "5",
+        email: "amanda@empresa.com.br",
+        nome: "Amanda",
+        senha: "789101112"
+
+    },
+    {
+        id: "6",
+        email: "rebeva@empresa.com.br",
+        nome: "Rebeca Silva",
+        senha: "789101112"
+    },
+
+
+]
+
+export default usuarios
+
